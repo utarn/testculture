f61ae496129edfd689cedfc75d0e8424da8ef06f
Utarn Buranasaksri <utarn@zarimpun.com>
2019-11-12 06:42:19 +0700

    second

--- testcultureinfo/Data/ApplicationDbContext.cs
+++ testcultureinfo/Data/ApplicationDbContext.cs
@@ -8,9 +8,16 @@ namespace testcultureinfo.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Sample> Samples { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+    }
+
+    public class Sample
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
---
+++ testcultureinfo/Migrations/20191111234203_second.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace testcultureinfo.Migrations
+{
+    public partial class second : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Samples",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Samples", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Samples");
+        }
+    }
+}
